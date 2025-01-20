ce19acd1cfd9433ac7adb3d2e752c1f572556ad7
Felipe Faria <2829082+synchronizing@users.noreply.github.com>
2025-01-19 20:33:08 -0500

    Fixed release.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="soltxs",
-    version="1.0.0",
+    version="1.0.1",
     author="Quick Vectors",
     author_email="felipe@qvecs.com",
     description="Solana transaction normalizer, parser, and resolver.",
